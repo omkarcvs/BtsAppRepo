abd7a44f32b1588d1a4f066b78f87bba8bfc8c41
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-05 11:20:12 +0530

    Final second

--- BtsApp/src/main/java/mypack/Final.java
+++ BtsApp/src/main/java/mypack/Final.java
@@ -1,10 +1,9 @@
 package mypack;
 
-public class Final {
+public class Final { 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("");
 	}
 
 }
