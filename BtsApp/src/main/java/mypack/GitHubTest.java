5d77e09f4a61fa9d12813eda7ece1adf1c0c3a0f
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-05 13:18:44 +0530

    Tesing more file

--- BtsApp/src/main/java/mypack/GitHubTest.java
+++ BtsApp/src/main/java/mypack/GitHubTest.java
@@ -5,6 +5,7 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("ttt"); 
 		System.out.println("ttt"); 
+		System.out.println("Testing"); 
 	}
 
 }
--- BtsApp/src/main/java/mypack/Terst.java
+++ BtsApp/src/main/java/mypack/Terst.java
@@ -1,5 +1,7 @@
 package mypack;
 
 public class Terst {  
-
+    public static void main(String[] args) {
+		System.out.println("Test : ");
+	}
 }
