548bc775fc9bec99869ac130b769ec5e718ed18f
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-05 11:13:23 +0530

    ttt

--- BtsApp/src/main/java/mypack/GitHubTest.java
+++ BtsApp/src/main/java/mypack/GitHubTest.java
@@ -1,9 +1,9 @@
 package mypack;
 
-public class GitHubTest {
+public class GitHubTest { 
 
 	public static void main(String[] args) {
-		System.out.println("ttt");
+		System.out.println("ttt"); 
 	}
 
 }
